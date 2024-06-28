d23e3ddc122a62d44fbcbe7ea78e544d4a9e9359
Hineill <hineill.cespedes@pucp.edu.pe>
2024-06-27 19:18:30 -0500

    first commit

---
+++ src/test/java/com/example/gtics_lab10_20213704/GticsLab1020213704ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gtics_lab10_20213704;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GticsLab1020213704ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
